260d3b4b8117125b98157f35a31ef3e9e5a570ce
Luis Alejo <lou.m.alejo@gmail.com>
2020-11-15 22:48:34 -0600

    computer languages

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import language_timeline
 
 app = Flask(__name__)
 
@@ -6,7 +7,20 @@ app.config['SECRET_KEY'] = b'\xf6U\xb5\xfb\x0c\x17\xca\xd0\xb7|\xe6S\xe0\xa7\xfb
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    languages = language_timeline.language_timeline()
+    return render_template('home.html', languages=languages)
+
+@app.route('/page_info')
+def page_info():
+    page_info = request.url
+    return render_template('page_info.html', page_info=page_info)
+
+# @app.route('/languages')
+# def language_timeline():
+#     languages = language_timeline.language_timeline()
+#     return render_template()
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
