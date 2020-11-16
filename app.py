c3a59b7fcc115738fddb538e1aaf07cc6f5f6676
Luis Alejo <lou.m.alejo@gmail.com>
2020-11-15 21:58:22 -0600

    exersice 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,11 @@ app.config['SECRET_KEY'] = b'\xf6U\xb5\xfb\x0c\x17\xca\xd0\xb7|\xe6S\xe0\xa7\xfb
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/page_info')
+def page_info():
+    page_info = request.url
+    return render_template('page_info.html', page_info=page_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
